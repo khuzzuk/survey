bc3c20aa984f12c1b6685c099d0af09dd01e032b
Adrian Drabik <adriandrabik@gmail.com>
2017-12-04 23:47:57 +0100

    fixing files

--- src/main/java/pl/khuzzuk/survey/controllers/SurveyRepo.java
+++ src/main/java/pl/khuzzuk/survey/controllers/SurveyRepo.java
@@ -1,11 +1,12 @@
 package pl.khuzzuk.survey.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.khuzzuk.survey.surveys.SurveyPages;
 
-import java.io.File;
 import java.io.IOException;
 import java.rmi.RemoteException;
 
@@ -14,7 +15,8 @@ public class SurveyRepo {
     @RequestMapping("/survey1")
     public SurveyPages getLogicTest() throws RemoteException {
         try {
-            return new ObjectMapper().readValue(new File("logicTest.json"), SurveyPages.class);
+            Resource resource = new PathMatchingResourcePatternResolver().getResource("classpath:static/logicTest.json");
+            return new ObjectMapper().readValue(resource.getInputStream(), SurveyPages.class);
         } catch (IOException e) {
             e.printStackTrace();
             throw new RemoteException();
